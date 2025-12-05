f5a9b5302427a882f757d3f75e42f7b0aa44d01a
zgfpeter <zgfpeter@gmail.com>
2025-12-05 12:00:11 +0200

    Fixed cors origin paths

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "finance-dashboard-gules-omega.vercel.app",
+      "https://finance-dashboard-2t31vuh1k-zgfpeters-projects.vercel.app",
     ],
     credentials: true,
   })
